f9a188d2b83db4ca6cb05ab5dacfd69137efcb42
Stefan Nica <snica@suse.com>
2020-07-01 13:17:34 +0200

    Add SLES support to install_cert.sh
    Add SLES support for handling installing custom certificates in the
    install_cert.sh script.

--- make/photon/common/install_cert.sh
+++ make/photon/common/install_cert.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-if [ ! -f ~/ca-bundle.crt.original ]; then
-    cp /etc/pki/tls/certs/ca-bundle.crt ~/ca-bundle.crt.original
-fi
+if grep -q "Photon" /etc/lsb-release; then
 
-cp ~/ca-bundle.crt.original /etc/pki/tls/certs/ca-bundle.crt
+    if [ ! -f ~/ca-bundle.crt.original ]; then
+        cp /etc/pki/tls/certs/ca-bundle.crt ~/ca-bundle.crt.original
+    fi
 
-if [ "$(ls -A /harbor_cust_cert)" ]; then
-    if grep -q "Photon" /etc/lsb-release; then
+    cp ~/ca-bundle.crt.original /etc/pki/tls/certs/ca-bundle.crt
+
+    if [ "$(ls -A /harbor_cust_cert)" ]; then
         echo "Appending trust CA to ca-bundle ..."
         for z in /harbor_cust_cert/*; do
             case ${z} in
@@ -25,7 +26,26 @@ if [ "$(ls -A /harbor_cust_cert)" ]; then
             esac
         done
         echo "CA appending is Done."
-    else
-        echo "Current OS is not Photon, skip appending ca bundle"
     fi
+elif grep -q "SLES" /etc/os-release; then
+    if [ "$(ls -A /harbor_cust_cert)" ]; then
+        echo "Installing trust CA ..."
+        for z in /harbor_cust_cert/*; do
+            case ${z} in
+                *.crt | *.ca | *.ca-bundle | *.pem)
+                    if [ -d "$z" ]; then
+                        echo "$z is directory, skip it ..."
+                    else
+                        cp $z /etc/pki/trust/anchors
+                        echo " $z Appended ..."
+                    fi
+                    ;;
+                *) echo "$z is Not ca file ..." ;;
+            esac
+        done
+        /usr/sbin/update-ca-certificates
+        echo "CA installing is Done."
+    fi
+else
+    echo "Current OS is not supported, skip appending ca bundle"
 fi
