28e29cb9d91f503fbededaaa0de75c71d54346b1
Stefan Nica <snica@suse.com>
2020-10-28 16:05:22 +0100

    SUSE branding changes for the Harbor UI

--- src/portal/src/app/services/theme.ts
+++ src/portal/src/app/services/theme.ts
@@ -8,17 +8,10 @@ export interface ThemeInterface {
 
 export const THEME_ARRAY: ThemeInterface[] = [
     {
-        showStyle: "DARK",
-        mode: "LIGHT",
-        text: "APP_TITLE.THEME_LIGHT_TEXT",
-        currentFileName: "dark-theme.css",
-        toggleFileName: "light-theme.css",
-    },
-    {
-        showStyle: "LIGHT",
-        mode: "DARK", // show button icon
-        text: "APP_TITLE.THEME_DARK_TEXT", // show button text
-        currentFileName: "light-theme.css", // loaded current theme file name
-        toggleFileName: "dark-theme.css", // to toggle theme file name
+        showStyle: "SUSE",
+        mode: "SUSE",
+        text: "",
+        currentFileName: "suse-theme.css",
+        toggleFileName: "suse-theme.css",
     }
 ];
